7d3b36251d5c186fe3532d7ccfa93fa544eef2bd
Naina Mogha <92297363+Smiley-2003@users.noreply.github.com>
2024-11-22 14:15:24 +0530

    Create useRestaurant.js

---
+++ useRestaurant.js
@@ -0,0 +1,16 @@
+// useRestaurant.js
+import React, { useContext } from 'react';
+import { RestaurantContext } from '../Context/RestaurantContext'; // Import the correct context
+
+const useRestaurantContext = () => {
+    const restaurantContext = useContext(RestaurantContext);
+
+    if (!restaurantContext) {
+        // Handle missing context (optional)
+        throw new Error('RestaurantContext not found');
+    }
+
+    return restaurantContext;
+};
+
+export default useRestaurantContext;
